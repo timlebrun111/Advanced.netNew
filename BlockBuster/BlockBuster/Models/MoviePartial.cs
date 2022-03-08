4e081bc842e29c072a850c2bcf71c3c999aa7ea2
timlebrun111 <tdlebrun@email.neit.edu>
2022-03-08 18:29:24 -0500

    Week 9 Updated Work

---
+++ BlockBuster/BlockBuster/Models/MoviePartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuster.Models
+{
+    [ModelMetadataType(typeof(MovieMetaData))]
+    public partial class Movie
+    {
+    }
+
+    public class MovieMetaData
+    {
+        [Display(Name = "Release Year")]
+        public int ReleaseYear { get; set; }
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+        [Display(Name ="Director")]
+        public int DirectorId { get; set; }
+    }
+}
---
+++ BlockBuster/BlockBuster/Models/TransactionPartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuster.Models
+{
+    [ModelMetadataType(typeof(TransactionMetaData))]
+    public partial class Transaction
+    {
+    }
+
+    public class TransactionMetaData
+    {
+        [Display(Name = "Checked Out Date")]
+        public DateTime CheckedOutDate { get; set; }
+        [Display(Name ="Due Date")]
+        public DateTime DueDate { get; set; }
+        [Display(Name = "Checked In")]
+        public string CheckedIn { get; set; }
+    }
+}
